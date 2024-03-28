01603afa56bb4cf4735b338907223136a72d6e89
adelpro <adelpro@gmail.com>
2024-03-28 19:45:04 +0100

    - working on header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#35bac6",
+        secondary: "#6AD4DD",
+      },
     },
   },
   plugins: [],
